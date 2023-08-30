3027cceee9c8554dc5e534f78dd36b6528b372dc
Fredrik Sandkvist <fredrik.sandq@gmail.com>
2023-08-30 11:29:47 +0200

    rename curves

--- src/sf/curve.rs
+++ src/sf/curve.rs
@@ -1,6 +1,6 @@
 #[derive(Clone, Copy)]
 pub enum Curve {
-    Linear, Fast, Slow, FastSlow,
+    Linear, Early, Late, EarlyLate,
 }
 
 impl Curve {
@@ -8,11 +8,9 @@ impl Curve {
         match self {
             //level to curve point / percentage
             Curve::Linear   => vec![(1, 0), (20, 100)], //curve 0 (0x08)
-            Curve::Slow     => vec![(1, 0), (10, 20), (14, 40), (20, 100)], //curve 1 (0x0A)
-
-            //todo: switch names on these two? am i dumb??
-            Curve::Fast     => vec![(1, 0), ( 4, 50), ( 6, 70), (10, 90), (14, 95), (20, 100)], //curve 3 (0x10)
-            Curve::FastSlow => vec![(1, 0), ( 4, 30), ( 6, 40), (14, 60), (16, 70), (20, 100)], //curve 2 (0x1A)
+            Curve::Late     => vec![(1, 0), (10, 20), (14, 40), (20, 100)], //curve 1 (0x0A)
+            Curve::Early     => vec![(1, 0), ( 4, 50), ( 6, 70), (10, 90), (14, 95), (20, 100)], //curve 3 (0x10)
+            Curve::EarlyLate => vec![(1, 0), ( 4, 30), ( 6, 40), (14, 60), (16, 70), (20, 100)], //curve 2 (0x1A)
         }
     }
 }
